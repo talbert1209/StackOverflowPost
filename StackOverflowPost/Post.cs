b91386c99b3926d7ba80b5c1223e03b366cefc4f
Albert, Timothy <TAlbert@greatdentalplans.com>
2018-09-05 13:39:58 -0500

    Initial Commit

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    partial class Program
+    {
+        public class Post
+        {
+            private string _title;
+            private string _description;
+            private DateTime _dateCreated;
+            private int voteValue;
+
+            public void CreatePost(string postTitle, string postDescription)
+            {
+                _title = postTitle;
+                _description = postDescription;
+            }
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post();
+
+
+        }
+    }
+}
